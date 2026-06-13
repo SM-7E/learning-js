5e85e2dc5c686c0beac3c36a4a48601248bdc114
Sumit Singh <stks1134@gmail.com>
2026-06-13 15:06:12 +0530

    learned about , ethics how to write operations

--- 01-basics/03-conversionOperation.js
+++ 01-basics/03-conversionOperation.js
@@ -8,3 +8,13 @@ let cLogin = Boolean(isLogin)
 
 console.log(cLogin)
 console.log(typeof cLogin)
+
+
+/* operation ethics */
+
+console.log(2+"2");
+console.log("2"+2); // agar string first hai toh sabhi ko string k hisab se treat kiya jayaga phale 
+console.log(2 + 2+"2"); // agar string last hai toh phale addition kiya jayaga 
+console.log("2" + 1 + 2); // string phale toh string k tarha treat hoga no addtion of last two no 
+// better use paranthesis ()
+console.log((3+4)* 5 % 4 )
