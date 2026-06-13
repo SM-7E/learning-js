3320bb326d38fee716b3cc318d028e27bb8f818b
Sumit Singh <stks1134@gmail.com>
2026-06-13 13:59:35 +0530

    printed multiple , data members using tablular formate

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,7 @@
+const accountId = 332342
+let accountEmail = "sum@gmv.com"
+var accountPassword = "224824"
+accountCity = "Malabar"
+
+console.log("accountId")
+console.table([accountEmail ,accountId ,accountPassword , accountCity])
