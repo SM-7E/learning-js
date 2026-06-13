e3cf74e1d577ba32566bd2deb2198dd59527fa21
Sumit Singh <stks1134@gmail.com>
2026-06-13 14:50:51 +0530

    learn about type conversion

---
+++ 01-basics/03-conversionOperation.js
@@ -0,0 +1,10 @@
+let number = 33 
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+let isLogin = 1 
+let cLogin = Boolean(isLogin)
+
+console.log(cLogin)
+console.log(typeof cLogin)
