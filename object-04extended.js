b687d28df07dc6315ee3af1e540125208732e05d
Sumit Singh <stks1134@gmail.com>
2026-06-18 14:42:28 +0530

    learned many thing about object an all

---
+++ object-04extended.js
@@ -0,0 +1,12 @@
+const course =  {
+    coursename: "afroz l Moba " , 
+    price : "kabhi mil hi nahi sakta ", 
+    courseInstructor : "Vighnesh Bhai "
+
+}
+
+// console.log
+// (course.courseInstructor) ;
+const {courseInstructor : instructor /* any name you can give */ } = course
+console.log(instructor); // dstrusture 
+
